d4aad5d8eb493775f12ceeac110eb80892da64f5
Lucas Solares <solareslucas.figueroa@gmail.com>
2020-05-15 21:50:28 -0600

    Finished course

---
+++ src/routes/profile.svelte
@@ -0,0 +1,23 @@
+<script>
+    import Main from '../components/Main.svelte'
+</script>
+
+<style>
+
+    .Profile {
+
+        padding-top: 4em;
+
+    }
+
+</style>
+
+<svelte:head>
+    <title>Configuración del Perfil</title>
+</svelte:head>
+
+<Main>
+    <div class="Profile">
+        <h2>Configuración de Perfil</h2>
+    </div>
+</Main>
